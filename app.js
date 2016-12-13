6b3237d06f9cf9898e243c68905db09a32dcdd3a
andreea grigorescu <deea26@gmail.com>
2016-12-13 14:11:22 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes/routes");
+const contactRoutes = require("./routes/contact");
+const app = express();
+
+//view engine
+app.set('view engine', 'ejs');
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.use(routes);
+app.use(contactRoutes);
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
---
+++ controllers/contactController.js
@@ -0,0 +1,26 @@
+const Contact = require("../models/contact");
+const mailer = require("../lib/email");
+
+class contactController {
+    static sendContactRequest(req, res) {
+       try {
+           //console.log(req.body);
+           const contactRequest = new Contact(req.body);
+           mailer.sendEmail(contactRequest)
+                    .then (() => {
+                        console.log("It sent the email");
+                        res.redirect("/");
+                    })
+                    .catch(error => {
+                        console.log("It failed because "+ error);
+                        res.redirect("/");
+                    });
+       } catch (err) {
+           //the error message
+           console.log(err);
+           res.redirect("/");
+       }
+    }
+}
+
+module.exports = contactController;

54a146d8d064c97ff74e30496934676561267e55
andreea grigorescu <deea26@gmail.com>
2016-12-13 14:24:00 +0000

    changes the console log for the port

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.use(contactRoutes);
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is listening on port 3000");
+    console.log(`Server listening on port ${process.env.PORT || 3000}`);
 });
